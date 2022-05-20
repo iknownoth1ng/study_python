efed5c23258de493f76fc446079450a09b749dbc
lixinyu1 <lixinyu1@szkingdom.com>
2022-05-20 17:46:51 +0800

    元组

---
+++ basic/07_tuple_pratice.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   07_tuple_pratice.py
+@Time    :   2022/05/19 13:12:55
+@Author  :   owl
+@Email   :   xxxxx@163.com
+@Desp    :   元组
+'''
+
+# here put the import lib
