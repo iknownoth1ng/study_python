7e1744745920f49542a4688dc27d2f3b77552608
lixinyu1 <lixinyu1@szkingdom.com>
2022-06-09 18:34:30 +0800

    迭代器（下）、生成器

--- 02advanced/07_iterator.py
+++ 02advanced/07_iterator.py
@@ -19,6 +19,7 @@
 '''迭代是访问集合元素的一种方式
 '''
 from collections.abc import Iterable
+from collections.abc import Iterator
 nums = [11, 22, 33]
 for _ in nums:
     print(_)
@@ -39,18 +40,17 @@ print(isinstance(1, Iterable))
 print(isinstance(1.0, Iterable))
 
 # 4.迭代器
+# iter()获取迭代器
 print(type(nums))  # <class 'list'>
 nums_iter = iter(nums)
 print(type(nums_iter))  # <class 'list_iterator'>
-# 获取迭代器的数据
+# next()获取迭代器的数据
 num1 = next(nums_iter)
 print(num1)
 num2 = next(nums_iter)
 print(num2)
 num3 = next(nums_iter)
 print(num3)
-# num4=next(nums_iter) StopIteration
-# print(num4)
 '''
 小总结：
 for循环的过程可以通过上面的iter(和next()函数来实现
@@ -59,7 +59,72 @@ for循环的过程可以通过上面的iter(和next()函数来实现
 2,调用next(),将上一步得到的送代器进行取值
 3,将上一步取出来的值赋值给num这个变量
 4,执行for循环体中的代码,print(num)
-5,接下来重复执行2/3/4步
+5,接下来重复执行2/3/4步,当nums所有数据被获取后,再获取下一个数据时
+会报错StopIteration,但是for循环会自动处理异常,遇到异常StopIteration会自动结束循环
 '''
 for num in nums:
     print(num)
+# next()获取时的异常StopIteration
+# num4=next(nums_iter) StopIteration
+# print(num4)
+
+# 练习
+# 一.自己实现一个WhileTrue遍历列表
+# 1.定义一个列表
+lst = [1, 2, 3, 4, 5, 6]
+# 2.获取它的迭代器
+lst_iter = iter(lst)
+# 3.循环的方式获取迭代器里的值
+while True:
+    try:
+        num = next(lst_iter)
+        print(num)
+    except StopIteration as e:
+        break
+
+# 二.自定义迭代器
+# 有__iter__()方法的对象，就是一个可迭代对象
+
+
+class MyList(object):
+    def __init__(self) -> None:
+        self.container = []
+
+    def __iter__(self):
+        return MyIterator()
+
+    def add(self, item):
+        self.container.append(item)
+
+
+myList = MyList()
+myList.add(11)
+myList.add(22)
+myList.add(33)
+
+# 看对象里是否有__iter__()方法
+print('myList是否是可迭代对象：', isinstance(myList, Iterable))
+
+# 实现了__iter__()和__next__()方法的是迭代器
+
+
+class MyIterator(object):
+    def __init__(self) -> None:
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __next__(self):
+        print('haha')
+        return 100
+
+
+myList_iter = iter(myList)  # 会调用__iter__()方法
+num = next(myList_iter)  # 会调用__next__()方法
+print(num)
+# 迭代器一定是可迭代对象，可迭代对象不一定是迭代器。
+print('myList是否是可迭代对象：', isinstance(myList, Iterable))  # True
+print('myList是否是迭代器：', isinstance(myList, Iterator))  # False
+print('myList_iter是否是可迭代对象：', isinstance(myList_iter, Iterable))  # True
+print('myList_iter是否是迭代器：', isinstance(myList_iter, Iterator))  # True
---
+++ 02advanced/09_generator.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   09_generator.py
+@Time    :   2022/06/09 16:11:01
+@Author  :   owl
+@Email   :   xxxxx@163.com
+@Desp    :   生成器
+'''
+
+# here put the import lib
+def func(n):
+    x=0
+    i=0
+    a=2
+    b=1
+    while True:
+        if i>n:
+            break
+        y=a*x+b
+        temp=yield (x,y)
+        if temp:
+            a,b=temp
+        i+=1
+        x=y
+
+f=func(5)
+print(next(f))
+print(next(f))
+print(next(f))
+print(next(f))
+print(f.send((3,2)))
+
+
+# 获取一个素数序列
+def _odd_iter():  # 先构造一个从3开始的奇数序列
+    n = 1
+    while True:
+        n = n + 2
+        yield n
+
+def _not_divisible(n):  # 定义一个筛选函数
+    return lambda x: x % n > 0
+
+def primes():
+    yield 2
+    it = _odd_iter()  # 初始序列
+    while True:
+        n = next(it)  # 返回序列的第一个数
+        yield n
+        it = filter(_not_divisible(n), it)  # 构造新序列
+
+# 打印1000以内的素数:
+for n in primes():
+    if n < 10:
+        #print(n, end=' ')
+        print(n)
+    else:
+        break
+
+
+# g=(x for x in range(5))
+# print(g)
+# print(next(g))
+# print('------')
+# for i in g:
+#     print(i)
+# print('------')
+# for j in g:
+#     print('*')
+#     print(j)
