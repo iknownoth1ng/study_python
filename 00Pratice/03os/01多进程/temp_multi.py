e9f27a77a6f185b87e88128eadab9e0245bd376f
owl <lordli1997@163,com>
2022-11-10 00:30:06 +0800

    Process的一些方法

--- 00Pratice/03os/01多进程/temp_multi.py
+++ 00Pratice/03os/01多进程/temp_multi.py
@@ -1,27 +1,26 @@
 # 查看子进程的父进程id
-import os
-import time
 from multiprocessing import Process
+import time
+import os
 
-
-def task(name):
-    print("{} is running".format(name))
+def task():
+    print("{} is running, parent id is {}".format(os.getpid(),os.getppid()))
     time.sleep(3)
+    print("{} is done, parent id is {}".format(os.getpid(),os.getppid()))
 
 if __name__ == "__main__":
     __spec__ = "ModuleSpec(name='builtins', loader=<class '_frozen_importlib.BuiltinImporter'>)"
 
-    p1=Process(target=task,args=('子进程1',))
-    p2=Process(target=task,args=('子进程2',))
-    p3=Process(target=task,args=('子进程3',))
+    p=Process(target=task)
+    p.start() # 仅仅只是给操作系统发送了一个信号
+    p.terminate() # 发送中断信号
+    # print(p.is_alive()) # True
 
-    p1.start() # 仅仅只是给操作系统发送了一个信号
-    p2.start() 
-    p3.start() 
+    p.join() # 或者time.sleep(3) 等待操作系统中断该进程
+    print(p.is_alive()) # false
+    
+    p.join() # 等待子进程执行完毕
+    print('主进程')
 
-    # p1.join() # 等待子进程执行完毕
-    # p2.join() 
-    # p3.join() 
-    print('主进程{}，父id{}'.format(os.getpid(),os.getppid()))
-
+    print(p.name) # 未起名会默认Process-1
 
