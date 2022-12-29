3cf08dc5ca54662a290ec7162be713b0a36afb33
owl <lordli1997@163,com>
2022-12-29 19:02:15 +0800

    闭包、装饰器、异步编程等

---
+++ 00Pratice/02advanced/test_future.py
@@ -0,0 +1,17 @@
+import time
+from concurrent.futures import Future,ThreadPoolExecutor,ProcessPoolExecutor
+
+def func(value):
+    time.sleep(1)
+    print(value)
+
+# 创建线程池
+pool=ThreadPoolExecutor(max_workers=5)
+
+# 创建进程池
+# pool=ProcessPoolExecutor(max_workers=5)
+
+for i in range(10):
+    fut=pool.submit(func,i)
+    print(fut)
+    
