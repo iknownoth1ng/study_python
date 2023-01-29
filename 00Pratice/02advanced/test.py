95d7fd2148021f006c563806171dfb35f0ad2d12
owl <lordli1997@163.com>
2023-01-30 01:45:44 +0800

    mysql

--- 00Pratice/02advanced/test.py
+++ 00Pratice/02advanced/test.py
@@ -1,10 +1,34 @@
-def func():
-    print("enter func()")
+import os.path as path
+import pandas as pd
 
-a = 1
-b = 2
-import pdb
-pdb.set_trace()
-func()
-c = 3
-print(a + b + c)
+def assert_msg(condition, msg):
+    if not condition:
+        raise Exception(msg)
+
+
+def read_file(filename):
+    # 获得文件绝对路径
+    filepath = path.join(path.dirname(__file__), filename)
+
+    # 判定文件是否存在
+    assert_msg(path.exists(filepath), "文件不存在")
+
+    # 读取CSV文件并返回
+    return pd.read_csv(
+        filepath, index_col=0, parse_dates=True, infer_datetime_format=True
+    )
+
+
+BTCUSD = read_file("BTCUSD_GEMINI.csv")
+assert_msg(BTCUSD.__len__() > 0, "读取失败")
+print(BTCUSD.head())
+
+
+########## 输出 ##########
+# Time                 Symbol      Open      High       Low     Close     Volume
+# Date
+# 2019-07-08 00:00:00  BTCUSD  11475.07  11540.33  11469.53  11506.43  10.770731
+# 2019-07-07 23:00:00  BTCUSD  11423.00  11482.72  11423.00  11475.07  32.996559
+# 2019-07-07 22:00:00  BTCUSD  11526.25  11572.74  11333.59  11423.00  48.937730
+# 2019-07-07 21:00:00  BTCUSD  11515.80  11562.65  11478.20  11526.25  25.323908
+# 2019-07-07 20:00:00  BTCUSD  11547.98  11624.88  11423.94  11515.80  63.211972
