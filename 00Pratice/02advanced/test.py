104a5d07b230eaf6e6985e4a5f83639d30e2d5eb
owl <lordli1997@163.com>
2023-01-31 02:23:43 +0800

    📦 NEW: django

--- 00Pratice/02advanced/test.py
+++ 00Pratice/02advanced/test.py
@@ -1,34 +1,26 @@
-import os.path as path
-import pandas as pd
+import datetime
 
-def assert_msg(condition, msg):
-    if not condition:
-        raise Exception(msg)
+import peewee
+from peewee import *
 
-
-def read_file(filename):
-    # 获得文件绝对路径
-    filepath = path.join(path.dirname(__file__), filename)
-
-    # 判定文件是否存在
-    assert_msg(path.exists(filepath), "文件不存在")
-
-    # 读取CSV文件并返回
-    return pd.read_csv(
-        filepath, index_col=0, parse_dates=True, infer_datetime_format=True
-    )
+db = MySQLDatabase("editestdb", host="myos", user="editest", passwd="test1234")
+db.connect()
 
 
-BTCUSD = read_file("BTCUSD_GEMINI.csv")
-assert_msg(BTCUSD.__len__() > 0, "读取失败")
-print(BTCUSD.head())
+class Price(peewee.Model):
+    timestamp = peewee.DateField(default=datetime.date.today)
+    BTCUSD = peewee.FloatField()
+
+    class Meta:
+        database = db
 
 
-########## 输出 ##########
-# Time                 Symbol      Open      High       Low     Close     Volume
-# Date
-# 2019-07-08 00:00:00  BTCUSD  11475.07  11540.33  11469.53  11506.43  10.770731
-# 2019-07-07 23:00:00  BTCUSD  11423.00  11482.72  11423.00  11475.07  32.996559
-# 2019-07-07 22:00:00  BTCUSD  11526.25  11572.74  11333.59  11423.00  48.937730
-# 2019-07-07 21:00:00  BTCUSD  11515.80  11562.65  11478.20  11526.25  25.323908
-# 2019-07-07 20:00:00  BTCUSD  11547.98  11624.88  11423.94  11515.80  63.211972
+def test_peewee():
+    Price.create_table()
+    price = Price(BTCUSD="11234.56")
+    price.save()
+
+
+if __name__ == "__main__":
+    Price.drop_table()
+    test_peewee()
